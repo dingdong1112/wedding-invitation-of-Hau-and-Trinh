b06675627c008f8d6d9d4b81674bdea26d402e2f
Duy Hau <vunguyenduyhau@gmail.com>
2025-11-28 20:11:08 +0700

    hauvnd
    Updated

--- api/wishes/[id].js
+++ api/wishes/[id].js
@@ -15,7 +15,6 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     if (req.method === 'OPTIONS') return res.status(200).end();
-    if (req.method !== 'DELETE') return res.status(405).json({ error: 'Method Not Allowed' });
 
     // Lấy ID từ URL (Vercel tự động đặt tham số động vào req.query)
     const { id } = req.query; 
