d143595845666f2b77715a88a9b1c67afbb949a9
Duy Hau <vunguyenduyhau@gmail.com>
2025-11-29 07:20:43 +0700

    hauvnd
    updated

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -431,7 +431,7 @@ export const guest = (() => {
 
         // 5. Tải thư viện phụ trợ
         lib.load({
-            aos: false,
+            aos: true,
             confetti: serverConfig.confetti_enabled // Chỉ tải thư viện nếu được bật
         });
 
